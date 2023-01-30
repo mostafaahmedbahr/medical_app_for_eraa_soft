21490d3a5f99ca2c4af4718e80059bc24ec25445
mostafa <mustafa.bahr12323@gmail.com>
2023-01-30 19:18:53 +0200

    reports _screen

--- lib/screens/reports_screen.dart
+++ lib/screens/reports_screen.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medical_app_for_eraa_soft/bloc/cubit.dart';
+import 'package:medical_app_for_eraa_soft/widgets/custom_text.dart';
+
+import '../bloc/states.dart';
 class ReportsScreen extends StatelessWidget {
   const ReportsScreen ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text("ReportsScreen");
+    return Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+    child: BlocConsumer<AppCubit,AppStates>(
+        listener: (context,state){},
+    builder: (context,state){
+      var cubit = AppCubit.get(context);
+          return  Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomText(text:"All Patient",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                  CustomText(text:"${cubit.getAllPatientModel?.data?.data?.length}",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    textColor: Colors.green,
+                  ),
+                ],
+              ),
+            ],
+          );
+    }
+
+    ),
+
+    );
   }
 }
