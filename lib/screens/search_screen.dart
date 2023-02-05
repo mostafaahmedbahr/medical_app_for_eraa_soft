69d1a94174a0a91ab9a1e6caa9cd8321ebb4fb77
mostafa <mustafa.bahr12323@gmail.com>
2023-02-05 16:08:58 +0200

    search_done2

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medical_app_for_eraa_soft/bloc/cubit.dart';
@@ -6,6 +7,7 @@ import 'package:medical_app_for_eraa_soft/core/colors.dart';
 import 'package:page_transition/page_transition.dart';
 
 import '../widgets/custom_sized_box.dart';
+import '../widgets/custom_text.dart';
 import '../widgets/patient_widget.dart';
 import 'details_screen.dart';
 
@@ -18,69 +20,95 @@ class SearchScreen extends StatelessWidget {
       listener: (context,state){},
       builder: (context,state){
         var cubit = AppCubit.get(context);
-        return   Scaffold(
-          appBar: AppBar(
-            backgroundColor: AppColors.mainColor,
-          ),
-          body:state is SearchLoadingState ? Center(
-            child: CircularProgressIndicator(
-              color: AppColors.mainColor,
-            ),
-          ) :
-          cubit.searchModel!.data!.data!.isEmpty ? Center(
-            child: Text("No Result",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),),
-          ) : Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: ListView.separated(
-              itemCount: cubit.searchModel!.data!.data!.length,
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () async {
-                    Navigator.push(
-                        context, PageTransition(
-                        duration: const Duration(milliseconds: 400),
-                        reverseDuration: const Duration(milliseconds: 400),
-                        type: PageTransitionType.rightToLeft,
-                        child: DetailsScreen(
-                          name: "${cubit.searchModel!.data!.data![index].name}",
-                          dataOfBirth: "${cubit.searchModel!.data!
-                              .data![index].dateOfBirth}",
-                          diagnosis: "${cubit.searchModel!.data!.data![index]
-                              .diagnosis}",
-                          address: "${cubit.searchModel!.data!.data![index]
-                              .address}",
-                          visitTime: "${cubit.searchModel!.data!.data![index]
-                              .visitTime}",
-                          id: "${cubit.searchModel!.data!.data![index].id}",
-                        ),
-                        inheritTheme: true,
-                        ctx: context));
+        return   StreamBuilder<ConnectivityResult>(
+          stream: Connectivity().onConnectivityChanged,
+    builder: (context,snapshot) {
+
+            return snapshot.data == ConnectivityResult.none ?
+            Scaffold(
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/images/wifi.png",
+                      height: 100,),
+                    CustomSizedBox(
+                      height: 20,
+                    ),
+                    CustomText(text: "No Internet",
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      textColor: Colors.black87,),
+                  ],
+                ),
+              ),
+            ): Scaffold(
+              appBar: AppBar(
+                backgroundColor: AppColors.mainColor,
+              ),
+              body:state is SearchLoadingState ? Center(
+                child: CircularProgressIndicator(
+                  color: AppColors.mainColor,
+                ),
+              ) :
+              cubit.searchModel!.data!.data!.isEmpty ? Center(
+                child: Text("No Result",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),),
+              ) :
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ListView.separated(
+                  itemCount: cubit.searchModel!.data!.data!.length,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context, PageTransition(
+                            duration: const Duration(milliseconds: 400),
+                            reverseDuration: const Duration(milliseconds: 400),
+                            type: PageTransitionType.rightToLeft,
+                            child: DetailsScreen(
+                              name: "${cubit.searchModel!.data!.data![index].name}",
+                              dataOfBirth: "${cubit.searchModel!.data!
+                                  .data![index].dateOfBirth}",
+                              diagnosis: "${cubit.searchModel!.data!.data![index]
+                                  .diagnosis}",
+                              address: "${cubit.searchModel!.data!.data![index]
+                                  .address}",
+                              visitTime: "${cubit.searchModel!.data!.data![index]
+                                  .visitTime}",
+                              id: "${cubit.searchModel!.data!.data![index].id}",
+                            ),
+                            inheritTheme: true,
+                            ctx: context));
+                      },
+                      child: CustomPatientWidget(
+                        name: "${cubit.searchModel!.data!.data![index].name}",
+                        dataOfBirth: "${cubit.searchModel!.data!.data![index]
+                            .dateOfBirth}",
+                        diagnosis: "${cubit.searchModel!.data!.data![index]
+                            .diagnosis}",
+                        address: "${cubit.searchModel!.data!.data![index]
+                            .address}",
+                        visitTime: "${cubit.searchModel!.data!.data![index]
+                            .visitTime}",
+                        id: "${cubit.searchModel!.data!.data![index].id}",
+                      ),
+                    );
                   },
-                  child: CustomPatientWidget(
-                    name: "${cubit.searchModel!.data!.data![index].name}",
-                    dataOfBirth: "${cubit.searchModel!.data!.data![index]
-                        .dateOfBirth}",
-                    diagnosis: "${cubit.searchModel!.data!.data![index]
-                        .diagnosis}",
-                    address: "${cubit.searchModel!.data!.data![index]
-                        .address}",
-                    visitTime: "${cubit.searchModel!.data!.data![index]
-                        .visitTime}",
-                    id: "${cubit.searchModel!.data!.data![index].id}",
-                  ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return const CustomSizedBox(
-                  height: 15,
-                );
-              },),
-          ),
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const CustomSizedBox(
+                      height: 15,
+                    );
+                  },),
+              ),
+            );
+    }
+
         );
       },
 
